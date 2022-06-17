76b157dffeb13acb711d42a635c7cced3ac4fc02
akchindo <abdullahichindo16@gmail.com>
2022-06-17 18:18:38 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Header from "./components/Header";
+import Tasks from "./components/Tasks";
+import AddTask from "./components/AddTask";
+import { useEffect, useState } from "react";
 
-function App() {
+const App = () => {
+  const [showAddTask, setShowAddTask] = useState(false);
+
+  const [tasks, setTasks] = useState([]);
+
+  useEffect (() => {
+    const fetchTask = async () => {
+      const res = await fetch('http://localhost:5000/tasks')
+      const data = await res.json()
+
+
+      return data
+    }
+
+    fetchTask()
+  }, [])
+
+    // Fetch Tasks
+    const fetchTasks = async () => {
+      const res = await fetch('http://localhost:5000/tasks')
+      const data = await res.json()
+  
+      return data
+    }
+
+  const addTask = (task) => {
+    const id = Math.floor(Math.random() * 1000) + 1;
+    const newTask = { id, ...task };
+    setTasks([...tasks, newTask]);
+  };
+
+  // Delete Task
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id));
+  };
+
+  const toggleReminder = (id) => {
+    setTasks(
+      tasks.map((task) =>
+        task.id === id ? { ...task, reminder: !task.reminder } : task
+      )
+    );
+    // console.log(id)
+  };
+
+  // const toggleTask = () => {
+  //   setShowAddTask((prev) => !prev)
+  // }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <Header title='Task Tracker' onAdd={() => setShowAddTask(!showAddTask) } showAdd={showAddTask}/>
+     { showAddTask && <AddTask onAdd={addTask} />}
+    
+      {tasks.length > 0 ? (
+        <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
+      ) : (
+        "No Tasks to show"
+      )}
     </div>
   );
-}
+};
 
 export default App;
+
---
+++ src/components/Tasks.js
@@ -0,0 +1,16 @@
+import React, { useState } from "react";
+import Task from "./Task";
+
+const Tasks = ({ tasks , onDelete, onToggle }) => {
+  return (
+    <div>
+      {
+        tasks.map((task) => (
+            <Task key={task.id} task={task} del={ onDelete } onToggle={onToggle} />
+        ))
+      }
+    </div>
+  )
+}
+
+export default Tasks;
